4964b1c8a4e2aa2b175c9e6ad007483ea9102eea
gaizkadev <gaizkadev@gmail.com>
2023-01-12 13:22:35 +0100

    Create Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div className="flex items-center justify-between px-4 py-2">
+      <div>Blog VM</div>
+      <div>Menu</div>
+    </div>
+  );
+}
